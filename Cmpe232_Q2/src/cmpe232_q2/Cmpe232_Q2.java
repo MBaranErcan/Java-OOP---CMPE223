e11ab04f007d3b0767f8d1ce142583d797a0f932
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2023-10-07 10:26:07 +0300

    Add files via upload

---
+++ Cmpe232_Q2/src/cmpe232_q2/Cmpe232_Q2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cmpe232_q2;
+
+/**
+ *
+ * @author musta
+ */
+public class Cmpe232_Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
